063ad131a7c6ae31aecfcc02de6c5f7bfbd681cf
domzalex <alexdomzalski@gmail.com>
2022-08-28 23:05:53 -0400

    old dates filtered out

--- src/Events.js
+++ src/Events.js
@@ -4,15 +4,23 @@ function Events({allEvents}) {
 
     let eventList
 
+    const today = new Date()
+    const todayFormat = today.toDateString()
+
     if (allEvents) {
         eventList = allEvents.map(event => {
-            return (
-                <ul className="event" key={event._id}>
-                    <li className="event-date">{(event.date.split(' ', 4)[0] + ' - ' + event.date.split(' ', 4)[1] + ', ' + event.date.split(' ', 4)[2] + ', ' + event.date.split(' ', 4)[3])} - {event.time}</li>
-                    <li className="event-name">{event.name}</li>
-                    <li className="event-location">{event.location}</li>
-                </ul>
-            )  
+            let eventDate = new Date(event.date.split(' ', 4)[0] + ' ' + event.date.split(' ', 4)[1] + ' ' + event.date.split(' ', 4)[2] + ' ' + event.date.split(' ', 4)[3])
+            let dateFormat = eventDate.toDateString()
+            if (eventDate.getTime() >= (today.getTime() - 86400000)) {
+                return (
+                    <ul className="event" key={event._id}>
+                        <li className="event-date">{dateFormat} - {event.time}</li>
+                        <li className="event-name">{event.name}</li>
+                        <li className="event-location">{event.location}</li>
+                    </ul>
+                )
+            } 
+              
         })
     }
     return (
