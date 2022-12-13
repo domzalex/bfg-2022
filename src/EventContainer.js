5a4f66a8b3090b4cf2e4a931b0f9dd92aa25f3f3
domzalex <alexdomzalski@gmail.com>
2022-12-13 17:51:17 -0500

    removed weekly

--- src/EventContainer.js
+++ src/EventContainer.js
@@ -8,27 +8,27 @@ function EventContainer({weekly, events}) {
     const eventDiv = useRef()
     const mainHeader = useRef()
 
-    function toggleMain() {
-        eventDiv.current.style.transform = 'translateX(-100vw)'
-        eventToggleBackground.current.style.transform = 'translateX(50vw)'
-        mainHeader.current.style.borderBottom = 'solid 1px black'
-    }
-    function toggleWeekly() {
-        eventDiv.current.style.transform = 'translateX(0vw)'
-        eventToggleBackground.current.style.transform = 'translateX(0vw)'
-        mainHeader.current.style.borderBottom = 'solid 1px white'
-    }
+    // function toggleMain() {
+    //     eventDiv.current.style.transform = 'translateX(-100vw)'
+    //     eventToggleBackground.current.style.transform = 'translateX(50vw)'
+    //     mainHeader.current.style.borderBottom = 'solid 1px black'
+    // }
+    // function toggleWeekly() {
+    //     eventDiv.current.style.transform = 'translateX(0vw)'
+    //     eventToggleBackground.current.style.transform = 'translateX(0vw)'
+    //     mainHeader.current.style.borderBottom = 'solid 1px white'
+    // }
 
     return (
         <>
             <div className="event-container">
                 <div ref={eventToggleBackground} className="event-header-background"></div>
                 <div ref={mainHeader} className="main-event-header">
-                    <h1 onClick={toggleWeekly} className="weekly-title">Weekly Events</h1>
-                    <h1 onClick={toggleMain} className="main-title">Main Calendar</h1>
+                    {/* <h1 onClick={toggleWeekly} className="weekly-title">Weekly Events</h1> */}
+                    <h1 className="main-title">Main Calendar</h1>
                 </div>
                 <div ref={eventDiv} className="inner-event-container">
-                    <Weekly weeklies={weekly}/>
+                    {/* <Weekly weeklies={weekly}/> */}
                     <Events allEvents={events}/>
                 </div>
             </div>
